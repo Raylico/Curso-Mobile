5a8a980f2585b4c2ba8300b1ac219c3e7d909c28
Raylico <rayssajuliodesouza45@gmail.com>
2025-05-14 16:59:51 -0300

    14.05.2025

---
+++ PersistenciaDadosFlutter/SQLite/exemplo_sqlite/lib/models/nota_model.dart
@@ -0,0 +1,40 @@
+class Nota{
+  //atributos = colunasBD
+  final int? id;
+  final String titulo;
+  final String conteudo;
+
+  //construtor
+  Nota({this.id, required this.titulo, required this.conteudo});
+
+  //métodos
+
+  //converter o objeto <=> BD
+
+  //sempre que for ter banco de dados
+  //toMap Objeto -> BD
+  Map<String, dynamic> toMap(){
+    return {
+      "id":id, //o id, inicialmente pode ser nulo
+      "titulo":titulo,
+      "conteudo":conteudo,
+    };
+  }
+  //fromMap BD -> Objeto
+  factory Nota.fromMap(Map<String, dynamic> map){
+    return Nota(
+      id: map["id"] as int, //converte para int (usando cast)
+      titulo: map["titulo"] as String,
+      conteudo: map["conteudo"] as String);
+  }
+}
+
+
+
+
+
+
+
+
+
+
